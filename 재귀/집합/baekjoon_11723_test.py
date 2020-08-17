12146cbff5f2eb29d7b5333a54a219539bb3c1b2
Kwakcena <rhkrgudwh@gmail.com>
2020-08-17 23:18:51 +0900

    Pass] 백준 11723번 집합 문제 풀이
    비트 연산을 사용하여 부분집합을 표현한다.

--- 재귀/집합/baekjoon_11723_test.py
+++ 재귀/집합/baekjoon_11723_test.py
@@ -1,51 +1,26 @@
-def add(N, number):
-    N |= (1 << number)
-    return N
+import sys
 
-def remove(N, number):
-    N &= ~(1 << number)
-    return N
+n = 20
+m = int(sys.stdin.readline())
+s = 0
 
-def toggle(N, number):
-    N ^= (1 << number)
-    return N
-
-def check(N, number):
-    check = N & (1 << number)
-    return 1 if check else 0
-
-def all():
-    return 2097150
-
-def empty():
-    return 0
-
-if __name__ == "__main__":
-    N = int(input())
-    S = 0
-
-    while True:
-        if N == 0:
-            break
-        N -= 1
-        cmd, num = '', 0
-        command = input().split()
-
-        if len(command) == 2:
-            cmd, num = command[0], int(command[1])
+for _ in range(m):
+    operator, *num = sys.stdin.readline().split()
+    if len(num) > 0:
+        x = int(num[0]) - 1
+    if operator == "add":
+        s = (s | (1 << x))
+    elif operator == "remove":
+        s = (s & ~(1 << x))
+    elif operator == "check":
+        result = (s & (1 << x))
+        if result > 0:
+            sys.stdout.write('1\n')
         else:
-            cmd = command[0]
-
-        if cmd == "add":
-            S = add(S, num)
-        elif cmd == "remove":
-            S = remove(S, num)
-        elif cmd == "toggle":
-            S = toggle(S, num)
-        elif cmd == "all":
-            S = all()
-        elif cmd == "empty":
-            S = empty()
-        elif cmd == "check":
-            print(check(S, num))
-
+            sys.stdout.write('0\n')
+    elif operator == "toggle":
+        s = (s ^ (1 << x))
+    elif operator == "all":
+        s = (1 << n) - 1
+    elif operator == "empty":
+        s = 0
