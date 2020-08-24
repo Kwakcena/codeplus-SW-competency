d6d03ff97c1fe04b8d7b0dabbd586854fa83b9b1
Kwakcena <rhkrgudwh@gmail.com>
2020-08-24 13:57:03 +0900

    덱을 사용한 문제풀이
    두 개의 큐로 나누지 않고 하나의 큐를 반으로 나누어 앞쪽에는 순간이동에
    대한 걸린 시간을 추가하고, 큐의 뒷쪽에는 1초가 걸린 이동시간의 정점들을
    추가한다.
    
    어차피 bfs에서 현재 정점을 꺼낼 땐 큐의 맨 앞에서 빼기 때문에 우선적으로
    0초가 걸린 정점에 대한 처리를 진행한다.

--- 그래프/숨바꼭질3/baekjoon-13549.py
+++ 그래프/숨바꼭질3/baekjoon-13549.py
@@ -5,7 +5,7 @@ MAX = 200000
 
 def bfs(n, k):
     dist = [-1] * MAX
-    q, next_q = deque(), deque()
+    q = deque()
     q.append(n)
     dist[n] = 0
 
@@ -13,19 +13,16 @@ def bfs(n, k):
         now = q.popleft()
         if now * 2 < MAX and dist[now * 2] == -1:
             dist[now * 2] = dist[now]
-            q.append(now * 2)
+            q.appendleft(now * 2)
         if now - 1 >= 0 and dist[now - 1] == -1:
             dist[now - 1] = dist[now] + 1
-            next_q.append(now - 1)
+            q.append(now - 1)
         if now + 1 < MAX and dist[now + 1] == -1:
             dist[now + 1] = dist[now] + 1
-            next_q.append(now + 1)
-        if not q:
-            q = next_q
-            next_q = deque()
+            q.append(now + 1)
 
     return dist[k]
-  
+
 
 def solution(n, k):
     return bfs(n, k)
