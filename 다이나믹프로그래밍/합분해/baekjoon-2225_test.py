14c2dbbf2a8cad73211c259616055f1454f78269
Kwakcena <rhkrgudwh@gmail.com>
2020-10-07 12:30:09 +0900

    시간 복잡도 줄이기
    계산식을 바꿔 시간 복잡도를 줄인다.

--- 다이나믹프로그래밍/합분해/baekjoon-2225_test.py
+++ 다이나믹프로그래밍/합분해/baekjoon-2225_test.py
@@ -1,4 +1,4 @@
-# from pprint import pprint
+from pprint import pprint
 
 
 def test_solution():
@@ -8,17 +8,16 @@ def test_solution():
 def solution(n, k):
     mod = 10 ** 9
     d = [[0] * (n + 1) for _ in range(k + 1)]
-    # pprint(d, indent=2, width=80)
+    pprint(d, indent=2, width=80)
 
     # 0부터 0까지의 정수 0개를 더해서 그 합이 0이 되는 경우의 수 = 1
     d[0][0] = 1
     for i in range(1, k + 1):
         for j in range(n + 1):
-            for l in range(j + 1):
-                d[i][j] += d[i - 1][j - l]
-            d[i][j] %= mod
+            d[i][j] = d[i - 1][j] + d[i][j - 1]
+        d[i][j] %= mod
 
-    # pprint(d, indent=2, width=80)
+    pprint(d, indent=2, width=80)
 
     return d[k][n]
 
