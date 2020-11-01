5c8e323ea4f742438b2f22e33e4e1ed2b04e2235
Kwakcena <rhkrgudwh@gmail.com>
2020-11-01 17:08:01 +0900

    Solve 스타트와 링크 using python
    스타트와 링크 문제를 python을 이용하여 문제를 풀었다.

--- 브루트포스/스타트와링크/baekjoon_14889.py
+++ 브루트포스/스타트와링크/baekjoon_14889.py
@@ -1,1 +1,25 @@
-# TODO ...
+from itertools import combinations
+
+n = int(input())
+s = [list(map(int, input().split())) for _ in range(n)]
+
+player = [i for i in range(1, n + 1)]
+min_value = float('inf')
+
+for combination in list(combinations(player, n // 2)):
+    start_team = combination
+    link_team = tuple(i for i in player if i not in start_team)
+
+    start_ans, link_ans = 0, 0
+    for start_index, link_index in zip(list(combinations(start_team, 2)),
+                                       list(combinations(link_team, 2))):
+        start_ans += (s[start_index[0] - 1][start_index[1] - 1] +
+                      s[start_index[1] - 1][start_index[0] - 1])
+        link_ans += (s[link_index[0] - 1][link_index[1] - 1] +
+                     s[link_index[1] - 1][link_index[0] - 1])
+
+    comp = abs(start_ans - link_ans)
+    if min_value > comp:
+        min_value = comp
+
+print(min_value)
