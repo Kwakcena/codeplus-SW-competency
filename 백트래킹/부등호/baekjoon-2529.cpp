f82fbd65ffb40a9b9309cdd9694871df57312c02
Kwakcena <rhkrgudwh@gmail.com>
2020-11-03 20:20:48 +0900

    First commit

---
+++ 백트래킹/부등호/baekjoon-2529.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<tuple>
+#include<deque>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+  
+  return 0;
+}
