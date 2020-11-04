8bff4a236500bdf091ee57cce0b4e7f5b24d36a7
Kwakcena <rhkrgudwh@gmail.com>
2020-11-04 19:03:32 +0900

    모든 경우를 탐색하는 브루트 포스 방법
    재귀를 이용하지만 순열을 완성한 다음 부등호 관계를 확인하기 때문에 모든
    경우를 탐색하여 이미 중간에 불가능함이 판단되더라도 계속해서 탐색한다.

--- 백트래킹/부등호/baekjoon-2529.cpp
+++ 백트래킹/부등호/baekjoon-2529.cpp
@@ -1,6 +1,6 @@
-#include<cstdio>
-#include<cstring>
+#include<iostream>
 #include<vector>
+#include<string>
 #include<tuple>
 #include<deque>
 #include<queue>
@@ -8,7 +8,46 @@
 
 using namespace std;
 
+int k;
+bool check[10];
+char oper[10];
+vector<string> ans;
+
+bool ok(string num) {
+  for(int i=0; i<k; i++) {
+    if(oper[i] == '<' && (int)num[i] > (int)num[i + 1]) return false;
+    if(oper[i] == '>' && (int)num[i] < (int)num[i + 1]) return false;
+  }
+  return true;
+}
+
+void go(int index, string num) {
+  if(index == k + 1) {
+    if(ok(num)) {
+      ans.push_back(num);
+    }
+    return;
+  }
+  for(int i=0; i<=9; i++) {
+    if(check[i]) continue;
+    check[i] = true;
+    go(index + 1, num+to_string(i));
+    check[i] = false;
+  }
+}
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
   
+  cin >> k;
+  string num;
+  for(int i = 0; i < k; i++) {
+    cin >> oper[i];  
+  }
+
+  go(0, num);
+  cout << ans[0] << endl;
+  cout << ans[ans.size() - 1] << endl;
   return 0;
 }
