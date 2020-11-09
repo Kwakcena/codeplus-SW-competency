faa44b7ba5ec2efda22c6117bbd39527d2bd6b9c
Kwakcena <rhkrgudwh@gmail.com>
2020-11-09 12:53:16 +0900

    Solve using CPP
    - C++을 이용하면 시간초과가 나지 않고 해결이 가능하다.

---
+++ 백트래킹/N-Queen/baekjoon-9663.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int answer;
+bool field[15][15];
+
+bool check(int row, int col) {
+  for(int i=0; i<row; i++) {
+    if(field[i][col]) {
+      return false;
+    }
+  }
+
+  int y = row - 1, x = col - 1;
+  while(y >= 0 && x >= 0) {
+    if(field[y][x]) {
+      return false;
+    }
+    y--, x--;
+  }
+
+  y = row - 1, x = col + 1;
+  while(y >= 0 && x < n) {
+    if(field[y][x]) {
+      return false;
+    }
+    y--, x++;
+  }
+  return true;
+}
+
+void calc(int row) {
+  if(row == n) {
+    answer++;
+    return;
+  }
+
+  for(int col = 0; col < n; col++) {
+    field[row][col] = true;
+    if(check(row, col)) {
+      calc(row + 1);
+    }
+    field[row][col] = false;
+  }
+}
+
+int main() {
+  cin >> n;
+  calc(0);
+  cout << answer;
+}
