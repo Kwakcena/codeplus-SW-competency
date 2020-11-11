c5ae82cdbe711fd9fd06877407c0e33c70346a93
Kwakcena <rhkrgudwh@gmail.com>
2020-11-11 17:02:37 +0900

    Refactoring
    다음 위치에 대한 접근을 for문으로 바꿈

--- 그래프/숨바꼭질4/baekjoon-13913.py
+++ 그래프/숨바꼭질4/baekjoon-13913.py
@@ -1,34 +1,36 @@
 from collections import deque
-from pprint import pprint
 
 MAX = 200001
-n, k = map(int, input().split())
-dist = {}
-q = deque()
-q.append(n)
-
-while q:
-    now = q.popleft()
-    if now == k:
-        break
-
-    if now - 1 >= 0 and now - 1 not in dist:
-        q.append(now - 1)
-        dist[now - 1] = now
-    if now + 1 < MAX and now + 1 not in dist:
-        q.append(now + 1)
-        dist[now + 1] = now
-    if now * 2 <= MAX and now * 2 not in dist:
-        q.append(now * 2)
-        dist[now * 2] = now
 
 
-ans = []
-start, end = k, n
-while start != end:
-    ans.append(start)
-    start = dist[start]
-ans.append(n)
+def bfs(n, k):
+    q = deque()
+    q.append(n)
+    dist = {}
+    while q:
+        now = q.popleft()
+        if now == k:
+            break
 
-print(len(ans) - 1)
-print(' '.join(map(str, ans[::-1])))
+        for next in [now - 1, now + 1, now * 2]:
+            if 0 <= next <= MAX and next not in dist:
+                q.append(next)
+                dist[next] = now
+    return dist
+
+
+def solution():
+    n, k = map(int, input().split())
+    move_dict = bfs(n, k)
+
+    ans = []
+    while k != n:
+        ans.append(k)
+        k = move_dict[k]
+    ans.append(n)
+
+    print(len(ans) - 1)
+    print(' '.join(map(str, ans[::-1])))
+
+
+solution()
