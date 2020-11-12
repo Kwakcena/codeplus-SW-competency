3b8a7cbb69d5745a34de20768cb48d2212c0a2d9
Kwakcena <rhkrgudwh@gmail.com>
2020-11-12 14:30:01 +0900

    CPP의 딕셔너리인 map을 이용한다
    map을 이용해서 이동한 경로를 저장하여 문제 풀이

--- 그래프/숨바꼭질4/baekjoon-13913.cpp
+++ 그래프/숨바꼭질4/baekjoon-13913.cpp
@@ -1,62 +1,76 @@
-#include<iostream>
-#include<vector>
-#include<cstring>
-#include<string>
-#include<tuple>
-#include<deque>
-#include<queue>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <tuple>
+#include <deque>
+#include <queue>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
 int n, k;
 const int MAX = 200001;
 int check[MAX];
-vector<string> dist(MAX);
+map<int, int> dist;
 
-void bfs() {
+void print(int from, int to) {
+  if(from != to) {
+    print(from, dist[to]);
+  }
+  cout << to << " ";
+}
+
+void bfs()
+{
   queue<int> q;
   q.push(n);
   memset(check, -1, sizeof(check));
   check[n] = 0;
-  dist[n] += to_string(n);
 
-  while(!q.empty()) {
+  while (!q.empty())
+  {
     int now = q.front();
-    if(now == k) {
+    if (now == k)
+    {
       break;
     }
     q.pop();
 
-    if(now - 1 >= 0 && check[now - 1] == -1) {
+    if (now - 1 >= 0 && check[now - 1] == -1)
+    {
       q.push(now - 1);
-      dist[now - 1] = dist[now] + ' ' + to_string(now - 1);
+      dist[now - 1] = now;
       check[now - 1] = check[now] + 1;
     }
 
-    if(now + 1 < MAX && check[now + 1] == -1) {
+    if (now + 1 < MAX && check[now + 1] == -1)
+    {
       q.push(now + 1);
-      dist[now + 1] = dist[now] + ' ' + to_string(now + 1);
+      dist[now + 1] = now;
       check[now + 1] = check[now] + 1;
     }
 
-    if(now * 2 <= MAX && check[now * 2] == -1) {
+    if (now * 2 <= MAX && check[now * 2] == -1)
+    {
       q.push(now * 2);
-      dist[now * 2] = dist[now] + ' ' + to_string(now * 2);
+      dist[now * 2] = now;
       check[now * 2] = check[now] + 1;
     }
   }
 }
 
-int main() {
+int main()
+{
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  
+
   cin >> n >> k;
   bfs();
 
   cout << check[k] << endl;
-  cout << dist[k];
+  print(n, k);
   return 0;
 }
