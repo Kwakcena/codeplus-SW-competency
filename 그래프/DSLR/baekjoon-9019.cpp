0f09fd037630bc8a69481e09914def6065789a1f
Kwakcena <rhkrgudwh@gmail.com>
2020-11-13 13:44:46 +0900

    시간초과 해결
    - 처음에 문자열로 문제를 풀었을 때 정수로 변환하고 다시 문자열로
      변환하는 그런 과정이 시간초과를 유발한 듯 하다.

--- 그래프/DSLR/baekjoon-9019.cpp
+++ 그래프/DSLR/baekjoon-9019.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
-#include<string>
 #include<cstring>
+#include<string>
 #include<tuple>
 #include<deque>
 #include<queue>
@@ -10,70 +10,51 @@
 using namespace std;
 
 const int MAX = 10000;
+int from[MAX];
+char how[MAX];
+int dist[MAX];
 
-int left_rotate(string &s) {
-  rotate(s.begin(), s.begin() + 1, s.end());
-  return stoi(s);
+void print(int n, int m) {
+  if(n == m) return;
+  print(n, from[m]);
+  cout << how[m];
 }
 
-int right_rotate(string &s) {
-  rotate(s.rbegin(), s.rbegin() + 1, s.rend());
-  return stoi(s);
-}
-
-void print(int n, int m, vector<pair<char, int>> &from) {
-  if(n != m) {
-    print(n, from[m].second, from);
+void check(int now, int next, char c, queue<int> &q) {
+  if(dist[next] == -1) {
+    q.push(next);
+    dist[next] = dist[now] + 1;
+    from[next] = now;
+    how[next] = c;
   }
-  cout << from[m].first;
 }
 
-void bfs(int a, int b, vector<pair<char, int>> &from) {
-  bool check[MAX];
-  memset(check, false, sizeof(check));
+void bfs(int a, int b) {
+  queue<int> q;
+  q.push(a);
   
-  queue<string> q;
-  q.push(to_string(a));
-  check[a] = true;
+  memset(dist, -1, sizeof(dist));
+  memset(from, -1, sizeof(from));
+  memset(how, 0, sizeof(how));
 
+  dist[a] = 0;
   while(!q.empty()) {
-    string s_now = q.front();
-
+    int now = q.front();
     q.pop();
-    int i_now = stoi(s_now);
-    if(i_now == b) {
-      break;
-    }
+
     int next;
-    next = i_now * 2 < MAX ? i_now * 2 : (i_now * 2) % MAX;
-    if(check[next] == false) {
-      check[next] = true;
-      from[next] = make_pair('D', i_now);
-      q.push(to_string(next));
-    }
+    next = (now * 2) % MAX;
+    check(now, next, 'D', q);
 
-    next = i_now - 1 > 0 ? i_now -1 : 9999;
-    if(check[next] == false) {
-      check[next] = true;
-      from[next] = make_pair('S', i_now);
-      q.push(to_string(next));
-    }
+    next = now - 1;
+    if(next == -1) next = 9999;
+    check(now, next, 'S', q);
 
-    next = left_rotate(s_now);
-    if(check[next] == false) {
-      check[next] = true;
-      from[next] = make_pair('L', i_now);
+    next = (now % 1000) * 10 + now / 1000;
+    check(now, next, 'L', q);
 
-      q.push(to_string(next));
-    }
-
-    next = right_rotate(s_now);
-    if(check[next] == false) {
-      check[next] = true;
-      from[next] = make_pair('R', i_now);
-
-      q.push(to_string(next));
-    }
+    next = (now / 10) + (now % 10) * 1000;
+    check(now, next, 'R', q);
   }
 }
 
@@ -87,12 +68,9 @@ int main() {
   for(int i=0; i<t; i++) {
     int a, b;
     cin >> a >> b;
-    vector<pair<char, int>> from(MAX);
-    bfs(a, b, from);
-    print(a, b, from);
+    bfs(a, b);
+    print(a, b);
     cout << '\n';
   }
-
-
   return 0;
 }
