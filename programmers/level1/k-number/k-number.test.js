ac78d2f6e0e381aa6ebe1a6345594ce4af2e4426
Kwakcena <rhkrgudwh@gmail.com>
2020-12-29 00:35:13 +0900

    Refactor: findNumber 함수로 몰아주기
    * 굳이 나눌 필요 없어서 findNumber에 몰아줬다.

--- programmers/level1/k-number/k-number.test.js
+++ programmers/level1/k-number/k-number.test.js
@@ -1,19 +1,15 @@
 const solution = (array, commands) => {
   return commands.reduce((answers, command) => {
-    const [start, end, number] = command;
-    const answer = findNumber(sliceArray(array, start, end), number);
+    const [start, end, index] = command;
+    const answer = findNumber(array, start, end, index);
     return [...answers, answer];
   }, [])
 }
 
-const sliceArray = (array, start, end) => {
+const findNumber = (array, start, end, index) => {
   return [...array]
     .slice(start - 1, end)
-    .sort((a, b) => a - b);
-}
-
-const findNumber = (array, index) => {
-  return array[index - 1];
+    .sort((a, b) => a - b)[index - 1];
 }
 
 test('solution', () => {
@@ -25,14 +21,8 @@ test('solution', () => {
   ).toEqual([5, 6, 3]);
 })
 
-test('slice array', () => {
-  expect(sliceArray([1, 5, 2, 6, 3, 7, 4], 2, 5)).toEqual([2, 3, 5, 6]);
-  expect(sliceArray([1, 5, 2, 6, 3, 7, 4], 4, 4)).toEqual([6]);
-  expect(sliceArray([1, 5, 2, 6, 3, 7, 4], 1, 7)).toEqual([1, 2, 3, 4, 5, 6, 7]);
-})
-
 test('find number', () => {
-  expect(findNumber([2, 3, 5, 6], 3)).toBe(5);
-  expect(findNumber([6], 1)).toBe(6);
-  expect(findNumber([1, 2, 3, 4, 5, 6, 7], 3)).toBe(3);
+  expect(findNumber([1, 5, 2, 6, 3, 7, 4], 2, 5, 3)).toBe(5);
+  expect(findNumber([1, 5, 2, 6, 3, 7, 4], 4, 4, 1)).toBe(6);
+  expect(findNumber([1, 5, 2, 6, 3, 7, 4], 1, 7, 3)).toBe(3);
 })
